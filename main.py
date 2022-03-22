a7cef22729d8456bd80a71c0530ae339ca9f9043
artemchege <artemchege@me.com>
2022-03-22 20:05:51 +0300

    login logic to User model

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import uvicorn
-from fastapi import FastAPI, status, Depends, HTTPException
+from fastapi import FastAPI, status, Depends
 from sqlalchemy.orm import Session
-# from datetime import timedelta
 
 from predict_divorce.schemas import User, UserCreated
 from schemas import Login
@@ -9,7 +8,6 @@ from database import get_db, Base, engine
 from crud import create_user_in_db, get_user_by_name
 from predict_divorce.router import router as predict_divorce_router
 from predict_divorce.models import User as UserModel
-from hashing import Hash
 from jwt import create_access_token, get_current_user
 
 app = FastAPI()
@@ -31,14 +29,7 @@ def get_logged_user(db: Session = Depends(get_db), user: User = Depends(get_curr
 
 @app.post('/login/', status_code=status.HTTP_200_OK, tags=['auth'])
 def login(login_request: Login, db: Session = Depends(get_db)):
-    # TODO: задвинуть бизнс логику в модель юзера
-    user = db.query(UserModel).filter(UserModel.name == login_request.name).first()
-    if not user:
-        raise HTTPException(detail=f'User does not exists', status_code=status.HTTP_400_BAD_REQUEST)
-
-    if not Hash.verify(plain_password=login_request.password, hashed_password=user.password):
-        raise HTTPException(detail=f'Invalid password', status_code=status.HTTP_400_BAD_REQUEST)
-
+    user = UserModel.get_user_by_name_and_password(name=login_request.name, db=db, password=login_request.password)
     access_token = create_access_token(data={"sub": user.name})
     return {"access_token": access_token, "token_type": "bearer"}
 
--- predict_divorce/models.py
+++ predict_divorce/models.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Column, Integer, DateTime, ForeignKey, String
 from sqlalchemy.sql import func
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Session
+
+from fastapi import status, HTTPException
 
 from database import Base
-# from models import User
+from hashing import Hash
 
 
 class DivorcePredictionRequest(Base):
@@ -43,4 +45,15 @@ class User(Base):
     password = Column(String)
 
     divorce_prediction_request = relationship('DivorcePredictionRequest', back_populates='creator')
+
+    @classmethod
+    def get_user_by_name_and_password(cls, name: str, password: str, db: Session):
+        user = db.query(cls).filter(cls.name == name).first()
+        if not user:
+            raise HTTPException(detail=f'User does not exists', status_code=status.HTTP_400_BAD_REQUEST)
+
+        if not Hash.verify(plain_password=password, hashed_password=user.password):
+            raise HTTPException(detail=f'Invalid password', status_code=status.HTTP_400_BAD_REQUEST)
+
+        return user
 
