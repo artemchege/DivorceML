0ef2c2eb640932669ac721c9e942a67da4dfa14e
artemchege <artemchege@me.com>
2022-03-26 15:41:46 +0300

    postgres password env fix

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgres')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', '1234')
-POSTGRES_HOST = os.environ.get('POSTGRES_PASSWORD', 'localhost')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'localhost')
 POSTGRES_PORT = os.environ.get('POSTGRES_PORT', '5432')
 POSTGRES_DB = os.environ.get('POSTGRES_DB', 'fastapi')
 
